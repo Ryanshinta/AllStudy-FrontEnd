8f4a243dda7c96263313886fe2e62796d406a593
Ryanshinta <ryanouyangyunru@gmail.com>
2022-07-26 06:23:49 +0000

    Header Layout

--- allstudy/src/App.js
+++ allstudy/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World!
-    </h1>
+    <div className="App">
+      <Header/>
+    </div>
   )
 
 }
---
+++ allstudy/src/components/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+    return(
+        <header className="flex items-center justify-between py-4 px-6 bg-black">
+            <a href="">
+                <img
+                    src="https://4m4you.com/wp-content/uploads/2020/06/logo-placeholder.png"
+                    alt=""
+                    className="w-32 h-12 object-contain" 
+                />
+            </a>
+
+        </header>
+    );
+}
+
+export default Header
