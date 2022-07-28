f68b79ed54ce5d61e973e44b3a73b7075c8c69c5
Ryanshinta <ryanouyangyunru@gmail.com>
2022-07-28 20:38:06 +0800

    change structure

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import LoginPage from './pages/Login';
 
 function App() {
     return (
-        <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gradient-to-r from-sky-500 to-indigo-500">
+        <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gradient-to-r from-sky-200 to-indigo-200">
             <div className="max-w-md w-full space-y-8 shadow-2xl p-5 bg-white">
                 <BrowserRouter>
                     <Routes>
