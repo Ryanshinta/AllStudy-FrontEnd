24a4bb0f17be125faddc546f5ac8444d17195002
Ryanshinta <ryanouyangyunru@gmail.com>
2022-08-02 07:43:48 +0000

    New room

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,3 +1,4 @@
+import NewRoom from "../components/CreateRoom/NewRoom";
 import Layout from "../components/Dashboard/Layout";
 import TodoList from "../components/TodoList/TodoList";
 
@@ -6,6 +7,12 @@ export default function DashBoard(){
         <>
             <Layout>
                 <TodoList/>
+                    <div >
+                        <div className="bottom-0 right-0 static">
+                            <NewRoom />
+                        </div>
+                        
+                    </div>
             </Layout>
         </>
     )
