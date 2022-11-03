c5319ef20e5515e291cc0db576a6d506019049e3
JIAHAO-0112 <jiahaochai0112@gmail.com>
2022-11-03 17:37:12 +0000

    chatroom - whatsapp layout

--- src/components/ChatRoom/ChatRoom.js
+++ src/components/ChatRoom/ChatRoom.js
@@ -13,7 +13,7 @@ const ChatRoom = () =>  {
 
                     <div className="w-1/3 border flex flex-col">
 
-                        <div className="py-2 px-3 bg-grey-lighter flex flex-row justify-between items-center">
+                        <div className="py-2 px-3 bg-stone-100 flex flex-row justify-between items-center">
                             <div>
                                 <img className="w-10 h-10 rounded-full" src="http://andressantibanez.com/res/avatar.png"/>
                             </div>
@@ -32,7 +32,7 @@ const ChatRoom = () =>  {
                         </div>
 
                         <div className="py-2 px-2 bg-grey-lightest">
-                            <input type="text" className="w-full px-2 py-2 text-sm" placeholder="Search or start new chat"/>
+                            <input type="text" className="w-full px-2 py-2 text-sm bg-stone-100" placeholder="Search or start new chat"/>
                         </div>
 
                         <div className="bg-grey-lighter flex-1 overflow-auto">
@@ -55,7 +55,7 @@ const ChatRoom = () =>  {
                                     </p>
                                 </div>
                             </div>
-                            <div className="bg-white px-3 flex items-center hover:bg-grey-lighter cursor-pointer">
+                            <div className="bg-white px-3 flex items-center hover:bg-stone-100 cursor-pointer">
                                 <div>
                                     <img className="h-12 w-12 rounded-full"
                                          src="https://www.biography.com/.image/t_share/MTE5NDg0MDU1MTIyMTE4MTU5/arnold-schwarzenegger-9476355-1-402.jpg"/>
@@ -74,7 +74,7 @@ const ChatRoom = () =>  {
                                     </p>
                                 </div>
                             </div>
-                            <div className="bg-white px-3 flex items-center hover:bg-grey-lighter cursor-pointer">
+                            <div className="bg-white px-3 flex items-center hover:bg-stone-100 cursor-pointer">
                                 <div>
                                     <img className="h-12 w-12 rounded-full"
                                          src="https://www.famousbirthdays.com/headshots/russell-crowe-6.jpg"/>
@@ -93,7 +93,7 @@ const ChatRoom = () =>  {
                                     </p>
                                 </div>
                             </div>
-                            <div className="bg-white px-3 flex items-center hover:bg-grey-lighter cursor-pointer">
+                            <div className="bg-white px-3 flex items-center hover:bg-stone-100 cursor-pointer">
                                 <div>
                                     <img className="h-12 w-12 rounded-full"
                                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGpYTzuO0zLW7yadaq4jpOz2SbsX90okb24Z9GtEvK6Z9x2zS5"/>
@@ -112,7 +112,7 @@ const ChatRoom = () =>  {
                                     </p>
                                 </div>
                             </div>
-                            <div className="bg-white px-3 flex items-center hover:bg-grey-lighter cursor-pointer">
+                            <div className="bg-white px-3 flex items-center hover:bg-stone-100 cursor-pointer">
                                 <div>
                                     <img className="h-12 w-12 rounded-full"
                                          src="https://www.biography.com/.image/t_share/MTE5NTU2MzE2MjE4MTY0NzQ3/harrison-ford-9298701-1-sized.jpg"/>
@@ -138,7 +138,7 @@ const ChatRoom = () =>  {
 
                     <div className="w-2/3 border flex flex-col">
 
-                        <div className="py-2 px-3 bg-grey-lighter flex flex-row justify-between items-center">
+                        <div className="py-2 px-3 bg-stone-100 flex flex-row justify-between items-center">
                             <div className="flex items-center">
                                 <div>
                                     <img className="w-10 h-10 rounded-full" src="https://darrenjameseeley.files.wordpress.com/2014/09/expendables3.jpeg"/>
@@ -170,7 +170,7 @@ const ChatRoom = () =>  {
                             <div className="py-2 px-3">
 
                                 <div className="flex justify-center mb-2">
-                                    <div className="rounded py-2 px-4" >
+                                    <div className="rounded py-2 px-4 bg-blue-100" >
                                         <p className="text-sm uppercase">
                                             February 20, 2018
                                         </p>
@@ -178,7 +178,7 @@ const ChatRoom = () =>  {
                                 </div>
 
                                 <div className="flex justify-center mb-4">
-                                    <div className="rounded py-2 px-4" >
+                                    <div className="rounded py-2 px-4 bg-blue-100" >
                                         <p className="text-xs">
                                             Messages to this chat and calls are now secured with end-to-end encryption. Tap for more info.
                                         </p>
@@ -186,106 +186,106 @@ const ChatRoom = () =>  {
                                 </div>
 
                                 <div className="flex mb-2">
-                                    <div className="rounded py-2 px-3">
-                                        <p className="text-sm text-teal">
+                                    <div className="rounded py-2 px-3 bg-stone-100">
+                                        <p className="text-sm text-blue-500">
                                             Sylverter Stallone
                                         </p>
                                         <p className="text-sm mt-1">
                                             Hi everyone! Glad you could join! I am making a new movie.
                                         </p>
-                                        <p className="text-right text-xs text-grey-dark mt-1">
+                                        <p className="text-right text-xs text-stone-400 mt-1">
                                             12:45 pm
                                         </p>
                                     </div>
                                 </div>
 
                                 <div className="flex mb-2">
-                                    <div className="rounded py-2 px-3">
-                                        <p className="text-sm text-purple">
+                                    <div className="rounded py-2 px-3 bg-stone-100">
+                                        <p className="text-sm text-blue-500">
                                             Tom Cruise
                                         </p>
                                         <p className="text-sm mt-1">
                                             Hi all! I have one question for the movie
                                         </p>
-                                        <p className="text-right text-xs text-grey-dark mt-1">
+                                        <p className="text-right text-xs text-stone-400 mt-1">
                                             12:45 pm
                                         </p>
                                     </div>
                                 </div>
 
                                 <div className="flex mb-2">
-                                    <div className="rounded py-2 px-3">
-                                        <p className="text-sm text-orange">
+                                    <div className="rounded py-2 px-3 bg-stone-100">
+                                        <p className="text-sm text-blue-500">
                                             Harrison Ford
                                         </p>
                                         <p className="text-sm mt-1">
                                             Again?
                                         </p>
-                                        <p className="text-right text-xs text-grey-dark mt-1">
+                                        <p className="text-right text-xs text-stone-400 mt-1">
                                             12:45 pm
                                         </p>
                                     </div>
                                 </div>
 
                                 <div className="flex mb-2">
-                                    <div className="rounded py-2 px-3">
-                                        <p className="text-sm text-orange">
+                                    <div className="rounded py-2 px-3 bg-stone-100">
+                                        <p className="text-sm text-blue-500">
                                             Russell Crowe
                                         </p>
                                         <p className="text-sm mt-1">
                                             Is Andrés coming for this one?
                                         </p>
-                                        <p className="text-right text-xs text-grey-dark mt-1">
+                                        <p className="text-right text-xs text-stone-400 mt-1">
                                             12:45 pm
                                         </p>
                                     </div>
                                 </div>
 
                                 <div className="flex mb-2">
-                                    <div className="rounded py-2 px-3">
-                                        <p className="text-sm text-teal">
+                                    <div className="rounded py-2 px-3 bg-stone-100">
+                                        <p className="text-sm text-blue-500">
                                             Sylverter Stallone
                                         </p>
                                         <p className="text-sm mt-1">
                                             He is. Just invited him to join.
                                         </p>
-                                        <p className="text-right text-xs text-grey-dark mt-1">
+                                        <p className="text-right text-xs text-stone-400 mt-1">
                                             12:45 pm
                                         </p>
                                     </div>
                                 </div>
 
                                 <div className="flex justify-end mb-2">
-                                    <div className="rounded py-2 px-3">
+                                    <div className="rounded py-2 px-3 bg-blue-100">
                                         <p className="text-sm mt-1">
                                             Hi guys.
                                         </p>
-                                        <p className="text-right text-xs text-grey-dark mt-1">
+                                        <p className="text-right text-xs text-stone-400 mt-1">
                                             12:45 pm
                                         </p>
                                     </div>
                                 </div>
 
                                 <div className="flex justify-end mb-2">
-                                    <div className="rounded py-2 px-3">
+                                    <div className="rounded py-2 px-3 bg-blue-100">
                                         <p className="text-sm mt-1">
                                             Count me in
                                         </p>
-                                        <p className="text-right text-xs text-grey-dark mt-1">
+                                        <p className="text-right text-xs text-stone-400 mt-1">
                                             12:45 pm
                                         </p>
                                     </div>
                                 </div>
 
                                 <div className="flex mb-2">
-                                    <div className="rounded py-2 px-3">
-                                        <p className="text-sm text-purple">
+                                    <div className="rounded py-2 px-3 bg-stone-100">
+                                        <p className="text-sm text-blue-500">
                                             Tom Cruise
                                         </p>
                                         <p className="text-sm mt-1">
                                             Get Andrés on this movie ASAP!
                                         </p>
-                                        <p className="text-right text-xs text-grey-dark mt-1">
+                                        <p className="text-right text-xs text-stone-400 mt-1">
                                             12:45 pm
                                         </p>
                                     </div>
@@ -294,7 +294,7 @@ const ChatRoom = () =>  {
                             </div>
                         </div>
 
-                        <div className="bg-grey-lighter px-4 py-4 flex items-center">
+                        <div className="bg-stone-100 px-4 py-4 flex items-center">
                             <div>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path opacity=".45" fill="#263238" d="M9.153 11.603c.795 0 1.439-.879 1.439-1.962s-.644-1.962-1.439-1.962-1.439.879-1.439 1.962.644 1.962 1.439 1.962zm-3.204 1.362c-.026-.307-.131 5.218 6.063 5.551 6.066-.25 6.066-5.551 6.066-5.551-6.078 1.416-12.129 0-12.129 0zm11.363 1.108s-.669 1.959-5.051 1.959c-3.505 0-5.388-1.164-5.607-1.959 0 0 5.912 1.055 10.658 0zM11.804 1.011C5.609 1.011.978 6.033.978 12.228s4.826 10.761 11.021 10.761S23.02 18.423 23.02 12.228c.001-6.195-5.021-11.217-11.216-11.217zM12 21.354c-5.273 0-9.381-3.886-9.381-9.159s3.942-9.548 9.215-9.548 9.548 4.275 9.548 9.548c-.001 5.272-4.109 9.159-9.382 9.159zm3.108-9.751c.795 0 1.439-.879 1.439-1.962s-.644-1.962-1.439-1.962-1.439.879-1.439 1.962.644 1.962 1.439 1.962z"></path></svg>
                             </div>
